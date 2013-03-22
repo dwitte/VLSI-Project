28ea91144437914152bd766c48fcd7dc3dbb2862
Derek Witte <dwitte@mines.edu>
2013-03-22 13:48:38 -0600

    The netlist generation script now is able to write the input and output pins to the file. The Parts are not yet finished.

--- Netlister/generator.pl
+++ Netlister/generator.pl
@@ -18,20 +18,155 @@ while (<componentfile>)
 {
 	chomp;
 	$line = $_;
-	$line =~ s/(.*)\((.*)/$1 \( $2/;
-	$line =~ s/(.*)\)(.*)/$1 \) $2/;
-	$line =~ s/(.*),(.*)/$1 , $2/;
 	$line =~ s/^\s*(.*)\s*$/$1/;
+	$line =~ s/^(.*)\s\s(.*)$/$1 $2/g;
+	$line =~ s/^\/\/.*$//;
+	$line =~ s/\s*([;,=\{\}\(\)\[\]])\s*/ $1 /g;
+	$line =~ s/ +/ /g;
 	@tokens = split(/ /, $line);
 	foreach my $token (@tokens)
 	{
-		if($token ne "\n" or $token ne " " or $token ne "")
-		{
-#			push(@all_tokens, $token);
-			print "$token\n";
-		}
+		push(@all_tokens, $token);
 	}
 }
 
 close (componentfile);
 
+foreach my $token (@all_tokens)
+{
+	if($token =~ /\/\*/)
+	{
+		$in_comment = 1;
+	}
+	elsif ($token =~ /\*\// and $in_comment eq 1)
+	{
+		$in_comment = 0;
+		next;	
+	}
+	
+	if($in_comment ne 1)
+	{
+		push(@clean_tokens, $token);;
+	}
+}
+
+#Write all components to the output netlist file.
+open (netlistfile, ">../chips/$platform/netlists/$component.net");
+
+#Get input
+$token = shift(@clean_tokens);
+if($token eq "CHIP")
+{
+	#Call Chip function
+	parse_Chip();
+}
+else
+{
+	#Throw error
+	print "$token\n";
+}
+
+close (netlistfile);
+
+sub parse_Chip
+{
+	
+	$token = shift(@clean_tokens);
+	print netlistfile "$token Netlist file\n";
+	$token = shift(@clean_tokens);
+	print netlistfile "Pins\n{\n";
+
+	$token = shift(@clean_tokens);
+	if($token eq "IN")
+	{
+		#call Input Pin Funciton
+		parse_Inputs();
+	}
+	else
+	{
+		#Throw Error
+	}
+	
+	$token = shift(@clean_tokens);
+	if($token eq "OUT")
+	{
+		#call Output PIn Function
+		parse_Outputs();
+	}
+	else
+	{
+		#Throw error
+	}
+
+	print netlistfile "}\n\nParts\n{\n";
+	$token = shift(@clean_tokens);
+	if($token eq "Parts:" )
+	{
+		#call Parts Function;
+		parse_Parts();
+	}
+	print netlistfile "}\n\n";
+}
+
+sub parse_Inputs
+{
+	while($token ne ";")
+	{
+		$token = shift(@clean_tokens);
+		local $pin = $token;
+		$token = shift(@clean_tokens);
+		if($token eq '[')
+		{
+			$token = shift(@clean_tokens);
+			local $array_count = $token;
+			for($i = 0; $i < $array_count; $i++)
+			{
+				print netlistfile "\t$pin\[$i\] input \n";
+			}
+			$token = shift(@clean_tokens);
+		}
+		elsif($token eq ',')
+		{
+			print netlistfile "\t$pin\n";
+		}
+		else
+		{
+			#Throw Error
+		}
+		$token = shift(@clean_tokens);
+	}
+}
+
+sub parse_Outputs
+{
+	while($token ne ";")
+	{
+		$token = shift(@clean_tokens);
+		local $pin = $token;
+		$token = shift(@clean_tokens);
+		if($token eq '[')
+		{
+			$token = shift(@clean_tokens);
+			local $array_count = $token;
+			for($i = 0; $i < $array_count; $i++)
+			{
+				print netlistfile "\t$pin\[$i\] output \n";
+			}
+			$token = shift(@clean_tokens);
+		}
+		elsif($token eq ',')
+		{
+			print netlistfile "\t$pin\n";
+		}
+		else
+		{
+			#Throw Error
+		}
+		$token = shift(@clean_tokens);
+	}
+}
+
+sub parse_Parts
+{
+	
+}
